acb805c405ea9b399bc1b717a53a7f72cfeb6f5d
Juan Esteban <juanestban000@gmail.com>
2021-06-01 16:35:09 -0500

    Initializer the WebApp

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // dude if use the env in nextjs
+  // env: {
+  //   development: process.env.ENV_DEVELOPMENT,
+  //   local: process.env.ENV_LOCAL,
+  //   production: process.env.ENV_PRODUCTION,
+  // },
+  jsconfigPaths: true,
+}
