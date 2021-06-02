f8349b0ec55e9e889f167c56c7a0ecfa789dd5d2
Juan Esteban <juanestban000@gmail.com>
2021-06-02 15:30:56 -0500

    test: probando el pre-commit for linux using the chmod -x command

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Index() {
       <h3>env produccion: {process.env.production}</h3>
       <h3>environment: {process.env.NODE_ENV}</h3>
       <h3>env local: {process.env.local}</h3>
+      <p>testing pre-commit</p>
     </>
   )
 }
