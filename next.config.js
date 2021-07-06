48dcce14f811bea90a0057af37044572dab96bbf
Juan Esteban <juan_ep@corhuila.edu.co>
2021-07-06 11:10:53 -0500

    Feat: anadiendo un export de constante que tendra el estado del NODE_ENV y la url que tendra dependiendo de este estado NODE_ENV

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   env: {
-    development: process.env.ENV_DEVELOPMENT,
     local: process.env.ENV_LOCAL,
-    production: process.env.ENV_PRODUCCION,
+    URL_API:
+      process.env.NODE_ === 'development'
+        ? process.env.ENV_DEVELOPMENT
+        : process.env.NODE_ === 'test'
+        ? process.env.ENV_TEST
+        : process.env.ENV_PRODUCCION,
+    NODE_: process.env.NODE_,
   },
   jsconfigPaths: true,
 }
